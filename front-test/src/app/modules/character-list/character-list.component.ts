057e927414a67d1f831800ff41641ca8757df869
ariannmichael <ariann.farias@ccc.ufcg.edu.br>
2019-12-28 14:11:57 -0300

    Filter Character Service

--- front-test/src/app/modules/character-list/character-list.component.ts
+++ front-test/src/app/modules/character-list/character-list.component.ts
@@ -1,8 +1,9 @@
+import { FilterCharacterService } from './../../core/service/filter-character.service';
 import { CharacterFilterOptions } from './../../shared/model/character-filter-options.model';
 import { Component, OnInit } from '@angular/core';
 import { Character } from 'src/app/shared/model/character.model';
 import { CharacterService } from 'src/app/core/service/character.service';
-import { Observable, Subscriber, concat } from 'rxjs';
+import { Observable, Subscriber, concat, forkJoin } from 'rxjs';
 
 @Component({
   selector: 'app-character-list',
@@ -27,7 +28,10 @@ export class CharacterListComponent implements OnInit {
   startLimit = 0;
   endLimit = 8;
 
-  constructor(private characterService: CharacterService) { }
+  constructor(
+    private characterService: CharacterService,
+    private filterCharacterService: FilterCharacterService
+  ) { }
 
   ngOnInit() {
     this.configLoadingDataStrategy();
@@ -113,71 +117,13 @@ export class CharacterListComponent implements OnInit {
       });
   }
 
-  /** Refactor after */
+  /** Filter the list of characters through service, using filter options */
   filterCharactersByOptions(filter: CharacterFilterOptions) {
-    console.log(filter);
     this.filteredCharacters = this.charactersList;
     this.numberOfCharactersFiltered = this.filteredCharacters.length;
 
-    if (this.isFilterNull(filter)) {
-      return;
-    }
-
-    if (filter.height) {
-      this.filteredCharacters = this.filteredCharacters.filter(character => {
-        return character.height === filter.height.toString();
-      });
-    }
-
-    if (filter.mass) {
-      this.filteredCharacters = this.filteredCharacters.filter(character => {
-        return character.mass === filter.mass.toString();
-      });
-    }
-
-    if (filter.hairColors) {
-      this.filteredCharacters = this.filteredCharacters.filter(character => {
-        return character.hair_color === filter.hairColors.toString();
-      });
-    }
-
-    if (filter.skinColors) {
-      this.filteredCharacters = this.filteredCharacters.filter(character => {
-        return character.skin_color === filter.skinColors.toString();
-      });
-    }
-
-    if (filter.eyeColors) {
-      this.filteredCharacters = this.filteredCharacters.filter(character => {
-        return character.eye_color === filter.eyeColors.toString();
-      });
-    }
-
-    if (filter.gender) {
-      this.filteredCharacters = this.filteredCharacters.filter(character => {
-        return character.gender === filter.gender.toString();
-      });
-    }
-
-    if (filter.birthYear) {
-      this.filteredCharacters = this.filteredCharacters.filter(character => {
-        return character.birth_year.slice(-3) === filter.birthYear.toString();
-      });
-    }
-
-    setTimeout(() => {
-      // for the pagination update the number of characters
-      this.numberOfCharactersFiltered = this.filteredCharacters.length;
-    });
-
-  }
-
-  /** Check if all filter's attributes is null */
-  private isFilterNull(filter: CharacterFilterOptions) {
-    return (filter === null) || (filter.height === null && filter.mass === null
-          && filter.hairColors === null && filter.skinColors === null
-          && filter.eyeColors === null && filter.birthYear === null
-          && filter.gender === null );
+    this.filterCharacterService.addCharacters(this.filteredCharacters);
+    this.filterCharacterService.filterCharacters(filter);
   }
 
 
